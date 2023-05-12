e2bd5591ece2dd0050e623c1e1f9d85182620d6e
xiangcitoZ <pzxiang1@gmail.com>
2023-05-12 14:21:21 +0200

    c

---
+++ NuggetRestauranteXZX/Models/BlobModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NuggetRestauranteXZX.Models
+{
+    public class BlobModel
+    {
+        public string Nombre { get; set; }
+        public string Url { get; set; }
+        public string Contenedor { get; set; }
+
+    }
+}
