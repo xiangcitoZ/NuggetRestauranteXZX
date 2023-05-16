a26a4dd955edf055c7f16a4db54983a0e2f537d6
xiangcitoZ <pzxiang1@gmail.com>
2023-05-16 09:30:33 +0200

    c

---
+++ NuggetRestauranteXZX/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NuggetRestauranteXZX.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }    
+        public string Password { get; set; }
+
+
+
+    }
+}
---
+++ NuggetRestauranteXZX/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcRepasoSegundoExam.Models
+{
+    [Table("USUARIOSCUBO")]
+    public class Usuario
+    {
+        [Key]
+        [Column("ID_USUARIO")]
+        public int IdUsuario { get; set; }
+
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+      
+        [Column("EMAIL")]
+        public string Email { get; set; }
+
+
+        [Column("PASS")]
+        public string Password { get; set; }
+
+        [Column("IMAGEN")]
+        public string ImagenPerfil { get; set; }
+
+
+    }
+}
